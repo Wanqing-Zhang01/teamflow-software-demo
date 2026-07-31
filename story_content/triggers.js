bd8cb89224a9b4758b34c87131da07955827c455
Wanqing-Zhang01 <wanqing.zhang01@outlook.com>
2026-07-31 09:57:30 -0400

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,50 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5aYS9eV9w5H":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+window.Script1 = function()
+{
+  const target = object('6ji4ZfgyNPx');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6IbcgGHa5e2';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
